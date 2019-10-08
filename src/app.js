179345e1753f30cc2d0dd6032a66ad4897368a76
Meet Bhatt <meetbhatt23@gmail.com>
2019-10-08 14:02:13 +0530

    Add ThemeProvider to app.js and have it consume theme

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import { h, Component } from 'preact';
 import { Router } from 'preact-router';
 
-import Header from './components/header';
+import {ThemeProvider} from 'styled-components';
+
 import GlobalStyle from "../styled/GlobalStyles";
+import theme from "../styled/theme";
+
+import Header from './components/header';
+
 // Code-splitting is automated for routes
 import Home from './routes/home';
 import Profile from './routes/profile';
@@ -10,26 +15,28 @@ import TestPage from './routes/test-page.js';
 
 export default class App extends Component {
 
-	/** Gets fired when the route changes.
-	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
-	 *	@param {string} event.url	The newly routed URL
-	 */
-	handleRoute = e => {
-		this.currentUrl = e.url;
-	};
+    /** Gets fired when the route changes.
+     *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
+     *	@param {string} event.url	The newly routed URL
+     */
+    handleRoute = e => {
+        this.currentUrl = e.url;
+    };
 
-	render() {
-		return (
-			<div id="app">
-				<GlobalStyle/>
-				<Header />
-				<Router onChange={this.handleRoute}>
-					<Home path="/" />
-					<Profile path="/profile/" user="me" />
-					<Profile path="/profile/:user" />
-					<TestPage path="/test-page"/>
-				</Router>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <ThemeProvider theme={theme}>
+                <div id="app">
+                    <GlobalStyle/>
+                    <Header />
+                    <Router onChange={this.handleRoute}>
+                        <Home path="/" />
+                        <Profile path="/profile/" user="me" />
+                        <Profile path="/profile/:user" />
+                        <TestPage path="/test-page"/>
+                    </Router>
+                </div>
+            </ThemeProvider>
+        );
+    }
 }
