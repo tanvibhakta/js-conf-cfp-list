20ae7807a94ab34b703082683223e2d1225bfc3c
Meet Bhatt <meetbhatt@obvious.in>
2019-10-08 13:53:43 +0530

    Add GlobalStyles component and theme

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ import { h, Component } from 'preact';
 import { Router } from 'preact-router';
 
 import Header from './components/header';
-
+import GlobalStyle from "../styled/GlobalStyles";
 // Code-splitting is automated for routes
 import Home from './routes/home';
 import Profile from './routes/profile';
 import TestPage from './routes/test-page.js';
 
 export default class App extends Component {
-	
+
 	/** Gets fired when the route changes.
 	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
 	 *	@param {string} event.url	The newly routed URL
@@ -21,6 +21,7 @@ export default class App extends Component {
 	render() {
 		return (
 			<div id="app">
+				<GlobalStyle/>
 				<Header />
 				<Router onChange={this.handleRoute}>
 					<Home path="/" />
