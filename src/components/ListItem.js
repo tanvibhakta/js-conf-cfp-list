7dd80f1b5a622d74b4d2cfed50e3fa09a6c939b1
Tanvi Bhakta <tanvibhakta@gmail.com>
2019-10-09 14:47:16 +0530

    Add button to list item

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,13 +3,15 @@ import LinkButton from "./LinkButton";
 const ListItemContainer = styled.li`
  `
 
-const ListItem = (props) => (
-    <ListItemContainer>
+const ListItem = (props) => {
+    // TODO: waft checks date for cfp and conf happening with current date and adds badge accordingly
+    return (<ListItemContainer>
         <span>{props.dateStart}</span>
         <a href={props.website}>{props.name}</a>
         <span>{props.location.city}, {props.location.country}</span>
-
-    </ListItemContainer>
-);
+        <LinkButton />  {/*This needs an href based on what role it is, so we can't just pass the role*/}
+    </ListItemContainer> )
+};
 
 export default ListItem;
+
