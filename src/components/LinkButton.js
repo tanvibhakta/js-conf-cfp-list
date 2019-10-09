e7304aa66eba56aa4d7936ba3d31b41d99bba64a
Meet Bhatt <meetbhatt@obvious.in>
2019-10-09 16:42:19 +0530

    Add badges and relevant logic to list item

--- src/components/LinkButton.js
+++ src/components/LinkButton.js
@@ -4,12 +4,15 @@ const LinkContainer = styled.a`
   border-radius: 0.25rem;
   padding: 0.5rem 1rem;
   color: black;
-  background-color: aquamarine;
+  background-color: ${props=> props.type === 'openCfp' ? 'lavender' : ''};
+  background-color: ${props=> props.type === 'happeningNow' ? 'yellow' : ''};
+  background-color: ${props=> !props.type ? 'aquamarine' : ''};
+
 `
 
-const LinkButton = ({children, href}) => (
-    <LinkContainer href={href} role="button">
-        {children}
+const LinkButton = (props) => (
+    <LinkContainer {...props} role="button">
+        {props.children}
     </LinkContainer>
 );
 
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -5,12 +5,33 @@ const ListItemContainer = styled.li`
  `
 
 const ListItem = (props) => {
-    // TODO: waft checks date for cfp and conf happening with current date and adds badge accordingly
+
+    let badge =  {
+        type: undefined,
+        href: undefined,
+        text: undefined
+    };
+
+    let today = new Date().toISOString();
+
+    if(props.cfp && props.cfp.dateStart) {
+        if(props.cfp.dateStart <= today && today <= props.cfp.dateEnd) {
+            badge.type = "openCfp";
+            badge.href = props.cfp.cfpWebsite || props.website;
+            badge.text = "Call For Papers";
+        }
+        else if (props.dateStart <= today && today <= props.dateEnd) {
+            badge.type = "happeningNow";
+            badge.href = props.website;
+            badge.text = "Happening Now";
+        }
+    }
+
     return (<ListItemContainer>
         <span>{getDisplayDate(props.dateStart)}</span>
         <a href={props.website}>{props.name}</a>
         <span>{props.location.city}, {props.location.country}</span>
-        <LinkButton />  {/*This needs an href based on what role it is, so we can't just pass the role*/}
+        {badge.type ? (<LinkButton type={badge.type} href={badge.href}>{badge.text}</LinkButton>) : null}
     </ListItemContainer> )
 };
 
