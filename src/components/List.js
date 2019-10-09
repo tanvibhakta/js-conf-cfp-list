6f1a5528b9fea933648dbd535b55937659a20e3a
Tanvi Bhakta <tanvibhakta@gmail.com>
2019-10-09 15:14:01 +0530

    Sort conferences in ascending chronological order

--- src/components/List.js
+++ src/components/List.js
@@ -1,22 +1,21 @@
 import styled from "styled-components";
 import ConferenceData from "../conferences.json"
 import ListItem from "./ListItem";
+import {dynamicSort} from "./utils";
 
 const ListContainer = styled.ul`
  `
 
-const List = () => (
-    <ListContainer>
-        {
-            ConferenceData.conferences.map(conference => <ListItem {...conference}></ListItem>)
-        }
-    </ListContainer>
-);
 const List = ({chronologyType}) => {
+
+    //TODO: waft datesort into upcoming and past.
+
     return (
         <ListContainer>
             {
-                ConferenceData.conferences.map(conference => <ListItem {...conference}></ListItem>)
+                ConferenceData.conferences.sort(dynamicSort("dateStart")).map(
+                    conference => <ListItem {...conference}></ListItem>
+                )
             }
         </ListContainer>
     );
