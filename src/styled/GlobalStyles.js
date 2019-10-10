b6a7a94bb9905df545096b66b97d0222d4b2073f
Meet Bhatt <meetbhatt@obvious.in>
2019-10-10 15:30:48 +0530

    Add fonts and font theming

--- src/styled/GlobalStyles.js
+++ src/styled/GlobalStyles.js
@@ -5,6 +5,19 @@ const GlobalStyle = createGlobalStyle`
   font-family: 'Heebo';
   src: url('../assets/fonts/Heebo-Light.ttf') format('truetype');
 }
+@font-face {
+  font-family: 'Heebo';
+  src: url('../assets/fonts/Heebo-Bold.ttf') format('truetype');
+}
+@font-face {
+  font-family: 'Heebo';
+  src: url('../assets/fonts/Heebo-Medium.ttf') format('truetype');
+}
+
+@font-face {
+  font-family: 'Heebo';
+  src: url('../assets/fonts/Heebo-Regular.ttf') format('truetype');
+}
   body {
   font-family: 'Heebo', sans-serif;
   width: calc(100vw - 4rem);
--- src/styled/theme.js
+++ src/styled/theme.js
@@ -1,5 +1,11 @@
 const theme = {
  maxContainerSize: "1440px"
+ fontSizes: {
+  small: '0.875rem',
+  medium: '1rem',
+  large: '1.25rem',
+  xlarge: '1.5rem'
+ }
 };
 
 export default theme;
